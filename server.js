8d724a70f637bd5c599c11c7ce6cde0824fafebe
seforarodriguez <seforarodriguez13@gmail.com>
2016-03-29 12:38:30 -0500

    eslint errors fixed

---
+++ server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const userRoutes = require('./lib/user/user.routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'supersecret';
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.locals.title = '';
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+app.use(userRoutes);
+
+app.get('/', (req, res) => {
+  console.log(req.flash())
+  res.render('index', { message: req.flash() });
+});
+
+MONGOURL = process.env.MONGOURL || 'mongodb://localhost:27017/nodeauth';
+mongoose.connect(MONGOURL, (err) => {
+  if (err) throw err;
+  
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
